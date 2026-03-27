58264bda27cd6ab114bc64470909ef5e968cca5f
Gaziz <gazizvalitov230378@gmail.com>
2026-03-27 20:19:05 +0500

    relay server features

--- main.go
+++ main.go
@@ -3,82 +3,130 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	libp2p "github.com/libp2p/go-libp2p"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
+	"github.com/libp2p/go-libp2p/core/event"
 	network "github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/protocol"
 	"github.com/multiformats/go-multiaddr"
 )
 
-func getDefaultRelays() []peer.AddrInfo {
-	rawAddrs := []string{
-		"/ip4/134.209.139.213/tcp/4002/p2p/12D3KooWLr6iyQ5QDrBDvkRMCLagfDcdfPLG5bhsQkhCtwjjQ5Se",
-		// "/ip4/104.131.131.82/tcp/4001/p2p/12D3KooWGkNpEPYA2QKiRwySqfjvlhmoQY2Q5QX5Q5QX5Q5Q4Q",
-		// "/ip4/35.201.129.130/tcp/4001/p2p/12D3KooWJXmRaQfoKCcEe4Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q",
-		// "/ip4/54.213.123.123/tcp/4001/p2p/12D3KooWGf3Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q",
-		// "/ip4/18.222.123.123/tcp/4001/p2p/12D3KooWDnQ5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q5Q",
-	}
-
-	relayInfos := make([]peer.AddrInfo, 0, len(rawAddrs))
-
-	for _, s := range rawAddrs {
-		addr, err := multiaddr.NewMultiaddr(s)
-		if err != nil {
-			fmt.Printf("Ошибка парсинга адреса %s: %s\n", s, err)
-			continue
-		}
-
-		info, err := peer.AddrInfoFromP2pAddr(addr)
-		if err != nil {
-			fmt.Printf("Ошибка получения AddrInfo из %s: %s\n", s, err)
-			continue
-		}
-
-		relayInfos = append(relayInfos, *info)
-	}
-	return relayInfos
-}
-
 func main() {
 	ctx := context.Background()
+	var kadht *dht.IpfsDHT
+
 	h, err := libp2p.New(
+		libp2p.EnableRelay(),
 		libp2p.EnableHolePunching(),
-		libp2p.ForceReachabilityPrivate(),
-		libp2p.EnableAutoRelayWithStaticRelays(
-			getDefaultRelays(),
+		libp2p.EnableAutoRelayWithPeerSource(
+			func(context context.Context, num int) <-chan peer.AddrInfo {
+				ch := make(chan peer.AddrInfo, num)
+				go func() {
+					defer close(ch)
+					if kadht == nil {
+						return
+					}
+					for _, addr := range kadht.RoutingTable().GetPeerInfos() {
+						select {
+						case ch <- peer.AddrInfo{ID: addr.Id}:
+						case <-context.Done():
+							return
+						}
+					}
+				}()
+				return ch
+			},
 		),
 	)
 	if err != nil {
 		panic(err)
 	}
-	kadht, err := dht.New(ctx, h, dht.Mode(dht.ModeServer))
+
+	sub, err := h.EventBus().Subscribe([]interface{}{
+		new(event.EvtLocalAddressesUpdated),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	btsrDone := make(chan struct{})
+	go func() {
+		for e := range sub.Out() {
+			switch v := e.(type) {
+			case event.EvtLocalAddressesUpdated:
+				for _, addr := range v.Current {
+					if strings.Contains(addr.Address.String(), "p2p-circuit") {
+						close(btsrDone)
+						return
+					}
+				}
+			}
+		}
+	}()
+
+	kadht, err = dht.New(
+		ctx,
+		h,
+		dht.Mode(dht.ModeAutoServer),
+		dht.RoutingTableFilter(func(ht any, p peer.ID) bool {
+			return dht.PrivateRoutingTableFilter(ht, p) || dht.PublicRoutingTableFilter(ht, p)
+		}),
+	)
 	if err != nil {
 		panic(err)
 	}
 
 	for _, addr := range dht.GetDefaultBootstrapPeerAddrInfos() {
 		if err := h.Connect(ctx, addr); err != nil {
-			panic(fmt.Sprint("Ошибка подключения к bootstrap peer: ", err))
+			fmt.Println("Ошибка подключения к bootstrap peer: ", err)
 		}
 	}
 
+	fmt.Println("Ждем окончания процесса peer bootstrap...")
 	if err := kadht.Bootstrap(ctx); err != nil {
 		panic(err)
 	}
+	<-btsrDone
 
 	fmt.Println("Peer ID:", h.ID())
+
+	for _, addr := range h.Addrs() {
+		if strings.Contains(addr.String(), "p2p-circuit") {
+			fmt.Println(addr.String())
+		}
+	}
 
 	h.SetStreamHandler("/chat-msg/1.0.0", handleStream)
 	for {
 		var input string
-		fmt.Print("Введите PeerId: ")
+		fmt.Print("Введите PeerAddres: ")
 		fmt.Scan(&input)
-		peer, err := kadht.FindPeer(ctx, peer.ID(input))
+		addr, err := multiaddr.NewMultiaddr(input)
 		if err != nil {
-			panic(fmt.Sprint("Ошибка начала поиска пира в DHT: ", err))
+			fmt.Println("Ошибка преобрзования multiaddr: ", err)
+			return
 		}
-		fmt.Println("✅ Пир найден: ", peer)
+		peerId, _ := peer.Decode(string(input[strings.LastIndex(input, "/")+1:]))
+		if err := h.Connect(ctx, peer.AddrInfo{
+			ID:    peerId,
+			Addrs: []multiaddr.Multiaddr{addr},
+		}); err != nil {
+			fmt.Println("Ошибка подключения к пиру: ", err)
+			return
+		}
+		s, err := h.NewStream(ctx, peerId, protocol.ID("/chat-msg/1.0.0"))
+		if err != nil {
+			fmt.Println("Ошибка создания нового потока: ", err)
+			return
+		}
+		n, _ := s.Write([]byte("Hello!"))
+		buffer := make([]byte, 1024)
+		s.Read(buffer)
+		fmt.Println(string(buffer[:n]))
+		s.Close()
 	}
 }
 
